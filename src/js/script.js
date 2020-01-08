e645482cdd2d7ee2115382da57ae671068aa08de
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-01-08 13:54:03 +0100

    Add Links and Baners pages

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,130 @@
-console.log(document.getElementsByTagName('a'));
+const app = {
 
-const links = document.getElementsByTagName('a')
+    toogleMenu: function() {
+        document.querySelector('.hamburger').addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector('.side-nav').classList.toggle('not-active');
+            document.querySelector('.side-logo-icon').classList.toggle('not-active');
+            document.querySelector('.side-footer').classList.toggle('not-active');
+        });
+    },
 
-for( let i=0; i<links.length; i++) {
-    //remove active class
-    links[i].classList.remove('active');
+    showChart: function () {
+        var ctx = document.getElementById('myChart').getContext('2d');
 
-    //add acitve class
-    links[i].addEventListener('click', function() {
-        links[i].classList.toggle('active');
-    });
+        var chart = new Chart(ctx, {
+            // 1
+            type: 'bar',
+            data: {
+                // 2
+                labels: [ '01', '02', '03', '04', '05', '06', '07', '08', '09', '10' ],
+                // 3
+                datasets: [
+                    {
+                        // 4
+                        label: 'Signups',
+                        // 5
+                        backgroundColor: '#8DBEC8',
+                        borderColor: '#8DBEC8',
+                        // 6
+                        data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ]
+                    },
+                    {
+                        label: 'FTD',
+                        backgroundColor: '#F29E4E',
+                        borderColor: '#F29E4E',
+                        data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ]
+                    },
+                    {
+                        label: 'Earned',
+                        backgroundColor: '#71B374',
+                        borderColor: '#71B374',
+                        data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+                        // 7
+                        hidden: true
+                    }
+                ]
+            }
+        })
+    },
+
+    activePages: function () {
+        const links = document.querySelectorAll('.side-nav__item');
+
+        for (let link of links) {
+            console.log('link:',link)
+            link.addEventListener('click', function() {
+
+                for (let link of links) {
+                    link.classList.remove('side-nav__item-active');
+                }
+
+                this.classList.add('side-nav__item-active');
+
+                console.log(this);
+
+                const pageId= this.id;
+                const pageClassName = pageId.replace('','.')
+                
+                const activePages = document.querySelectorAll('.active');
+
+                for (let activePage of activePages) {
+                    activePage.classList.remove('active');
+                }
+
+                document.querySelector(pageClassName).classList.toggle('active');
+            })
+        }
+
+    },
+
+
+
+    init: function() {
+        const thisApp = this;
+        console.log('*** App starting ***');
+        console.log('thisApp:', thisApp);
+        
+        thisApp.toogleMenu();
+        thisApp.activePages();
+        thisApp.showChart();
+    },
+
 }
+
+app.init();
+ 
+ /*
+const titleClickHandler = function(event) {
+    event.preventDefault();
+    const clickedElement = this;
+    console.log('lala');
+    console.log(clickedElement);
+
+   
+    for(let activeLink of activeLinks) {
+        activeLink.classList.remove('active')
+    }
+
+    this.classList.add('active');
+
+    const articles = optArticleSelector;
+
+    for(let article of articles) {
+        article.classList.remove('.product .active')
+        
+    }
+ 
+    let idArticle = clickedElement.getAttribute('href').replace('#',"");
+    
+    document.getElementById(idArticle).classList.add('active');
+
+
+const links = document.querySelectorAll('.side-nav__link');
+
+    for (let link of links) {
+        link.addEventListener('click', titleClickHandler)
+    }
+
+}
+*/
