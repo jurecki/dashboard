8640073ddff9f3eac944db2698f1aea95d10f402
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-01-10 14:02:17 +0100

    add responsive code

--- src/js/script.js
+++ src/js/script.js
@@ -3,30 +3,27 @@ const app = {
     toogleMenu: function() {
         document.querySelector('.hamburger').addEventListener('click', function(e) {
             e.preventDefault();
-            document.querySelector('.side-nav').classList.toggle('not-active');
-            document.querySelector('.side-logo-icon').classList.toggle('not-active');
-            document.querySelector('.side-footer').classList.toggle('not-active');
+            document.querySelector('.sidebar').classList.toggle('not-active');
         });
+
     },
 
     showChart: function () {
         var ctx = document.getElementById('myChart').getContext('2d');
+        var parent = document.getElementById("chart");
+
+        ctx.canvas.width  = parent.offsetWidth;
+        ctx.canvas.height = parent.offsetHeight;
 
         var chart = new Chart(ctx, {
-            // 1
             type: 'bar',
             data: {
-                // 2
                 labels: [ '01', '02', '03', '04', '05', '06', '07', '08', '09', '10' ],
-                // 3
                 datasets: [
                     {
-                        // 4
                         label: 'Signups',
-                        // 5
                         backgroundColor: '#8DBEC8',
                         borderColor: '#8DBEC8',
-                        // 6
                         data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ]
                     },
                     {
@@ -40,7 +37,6 @@ const app = {
                         backgroundColor: '#71B374',
                         borderColor: '#71B374',
                         data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
-                        // 7
                         hidden: true
                     }
                 ]
